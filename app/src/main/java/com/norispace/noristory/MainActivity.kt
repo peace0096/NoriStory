66a3bc13d46046a1d68380115a979e54103e447e
peace0096 <peace0096@naver.com>
2021-05-08 21:17:09 +0900

    main gui

--- app/src/main/java/com/norispace/noristory/MainActivity.kt
+++ app/src/main/java/com/norispace/noristory/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.norispace.noristory
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
+    }
+
+    private fun init() {
+        val storyBtn = findViewById<Button>(R.id.storybtn)
+        storyBtn.setOnClickListener {
+            val intent = Intent(this, StoryMenuActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }

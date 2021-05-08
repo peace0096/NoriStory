d2bdc4bbdb08fc036d283d25d15b604c279ea66d
peace0096 <peace0096@naver.com>
2021-05-08 20:18:11 +0900

    splash

---
+++ app/src/main/java/com/norispace/noristory/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.norispace.noristory
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        init()
+    }
+
+    private fun init() {
+        val imageView = findViewById<ImageView>(R.id.imageView1)
+        imageView.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}

1f07679b58d15b607076a8aa733411978e65d7ec
Jaehyeon <jerry0906@naver.com>
2021-05-08 21:38:10 +0900

    OptionalActivity, SubjectActivity 버튼 활성화

--- app/src/main/java/com/norispace/noristory/StoryMenuActivity.kt
+++ app/src/main/java/com/norispace/noristory/StoryMenuActivity.kt
@@ -1,11 +1,30 @@
 package com.norispace.noristory
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class StoryMenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_story_menu)
+        init()
+    }
+
+    private fun init()
+    {
+        val OptinalBtn = findViewById<Button>(R.id.OptinalBtn)
+        val SubjectBtn = findViewById<Button>(R.id.SubjectBtn)
+
+        OptinalBtn.setOnClickListener {
+            val intent = Intent(this, OptionalActivity::class.java)
+            startActivity(intent)
+        }
+
+        SubjectBtn.setOnClickListener {
+            val intent = Intent(this, SubjectActivity::class.java)
+            startActivity(intent)
+        }
     }
 }

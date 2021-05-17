000aaa4f921ebb770561c5856a36153cf4c82547
주영환 <wnaltj@naver.com>
2021-05-17 23:33:47 +0900

    선택형 이야기 일부 구현

--- app/src/main/java/com/norispace/noristory/SunMoonActivity.kt
+++ app/src/main/java/com/norispace/noristory/SunMoonActivity.kt
@@ -23,22 +23,21 @@ class SunMoonActivity : AppCompatActivity() {
 
     private fun init(){
         var page =intent.getIntExtra("page",1)
-        var choice =intent.getIntExtra("choice",0)
-        setImg(page,choice)
+        setImg(page)//페이지 배경 이미지 출력
         binding.gotoMainBtn?.visibility=View.GONE
-        if(page==9) {
+        if(page==9) {//첫번째 선택 지점
             binding.nextButton?.visibility=View.GONE
             binding.choiceButton3?.visibility=View.GONE
             binding.choiceButton4?.visibility=View.GONE
             firstChoice(page)
 
-        }else if(page==14){
+        }else if(page==14){//두번째 선택 지점
             binding.choiceButton1?.visibility=View.GONE
             binding.choiceButton2?.visibility=View.GONE
             binding.nextButton?.visibility=View.GONE
             secondChoice(page)
         }
-        else {
+        else {//그 외 -> 선택 버튼 보이면 안됨
             binding.choiceButton1?.visibility=View.GONE
             binding.choiceButton2?.visibility=View.GONE
             binding.choiceButton3?.visibility=View.GONE
@@ -48,45 +47,43 @@ class SunMoonActivity : AppCompatActivity() {
             binding.apply {
 
 
-                exitButton?.setOnClickListener {
+                exitButton?.setOnClickListener {//이야기 나가기 버튼
                     val intent = Intent(this@SunMoonActivity, BooksActivity::class.java)
                     intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
                     startActivity(intent)
                 }
-                nextButton?.setOnClickListener {
+                nextButton?.setOnClickListener {//다음페이지 버튼
 
-                    if(page==21 || page==24||page==29){
+                    if(page==21 || page==24||page==29){//21, 24, 29페이지 에서 다음 페이지 버튼 누를 경우
                         binding.nextButton?.visibility=View.GONE
                         binding.postButton?.visibility=View.GONE
                         binding.exitButton?.visibility=View.GONE
-                        end()
-                    }else{
+                        end() // 이야기 끝났음을 알리는 페이지 출력 (sunmoonend)
+                    }else{//그 외 -> 페이지 1 증가
                         val intent = Intent(this@SunMoonActivity, SunMoonActivity::class.java)
                        page += 1
                         intent.putExtra("page", page)
-                        intent.putExtra("choice",choice)
                         startActivity(intent)
                     }
 
                 }
-                if (page == 1) {
+                if (page == 1) {//첫번째 페이지에서는 이전페이지 버튼 없음
                     postButton?.visibility = View.GONE
 
                 } else {
                     postButton?.setOnClickListener {
                         if(page!=1) {
                             val intent = Intent(this@SunMoonActivity, SunMoonActivity::class.java)
-                            if(page==25){
+                            if(page==25){//25페이지(피그마 참고)에서 이전 페이지 버튼 누르면 첫번째 선택 페이지로 이동
                                 page=9
-                            }else if(page==22){
+                            }else if(page==22){//22페이지(피그마 참고)에서 이전 페이지 버튼 누르면 두번째 선택 페이지로 이동
                                 page=14
-                            }else{
+                            }else{//그 외 -> 페이지 1 감소
                                 page -= 1
                             }
 
                             intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
                             intent.putExtra("page", page)
-                            intent.putExtra("choice", choice)
                             startActivity(intent)
                         }
                     }
@@ -94,7 +91,7 @@ class SunMoonActivity : AppCompatActivity() {
 
             }
     }
-    private fun end(){
+    private fun end(){//이야기가 끝났음을 알리고, 메인메뉴로 돌아가는 버튼 출력함
 
         val imgName = "sunmoonend"
         val id = resources.getIdentifier(imgName, "drawable", packageName)
@@ -107,21 +104,7 @@ class SunMoonActivity : AppCompatActivity() {
         }
     }
 
-    private fun setImg(page:Int,choice:Int){
-        //10페이지가 첫 번째 분기점
-//        if(page>=10){
-//            binding.apply{
-//                val imgName = "sunmoon" + page.toString() + "_"+choice.toString()
-//                val id = resources.getIdentifier(imgName, "drawable", packageName)
-//                imageView?.setImageResource(id)
-//            }
-//        }else {
-//            binding.apply {
-//                val imgName = "sunmoon" + page.toString()
-//                val id = resources.getIdentifier(imgName, "drawable", packageName)
-//                imageView?.setImageResource(id)
-//            }
-//        }
+    private fun setImg(page:Int){//배경 이미지 출력
         binding.apply{
                 val imgName = "sunmoon" + page.toString()
                 val id = resources.getIdentifier(imgName, "drawable", packageName)
@@ -131,9 +114,9 @@ class SunMoonActivity : AppCompatActivity() {
 
     private  fun firstChoice(page:Int){
         binding.apply {
-            choiceButton1?.visibility = View.VISIBLE
+            choiceButton1?.visibility = View.VISIBLE//첫번째 분기점 -> 선택버튼 1, 2
             choiceButton2?.visibility = View.VISIBLE
-            choiceButton1?.setOnClickListener {
+            choiceButton1?.setOnClickListener {//누르는 버튼에 따라 그에 맞는 페이지로 이동함(피그마 참고)
                 val intent = Intent(this@SunMoonActivity, SunMoonActivity::class.java)
                 intent.putExtra("page", 10)
                 //intent.putExtra("choice",1)
@@ -150,9 +133,9 @@ class SunMoonActivity : AppCompatActivity() {
     }
     private  fun secondChoice(page:Int){
         binding.apply {
-            choiceButton3?.visibility=View.VISIBLE
+            choiceButton3?.visibility=View.VISIBLE//두번째 분기점 -> 선택버튼 3, 4
             choiceButton4?.visibility=View.VISIBLE
-            choiceButton3?.setOnClickListener {
+            choiceButton3?.setOnClickListener {//누르는 버튼에 따라 그에 맞는 페이지로 이동함(피그마 참고)
                 val intent = Intent(this@SunMoonActivity, SunMoonActivity::class.java)
                 intent.putExtra("page", 15)
                 //intent.putExtra("choice",1)

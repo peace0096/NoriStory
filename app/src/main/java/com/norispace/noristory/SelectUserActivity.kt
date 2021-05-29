8d14be252abc3256343ed174047a32ce10a5cad9
주영환 <wnaltj@naver.com>
2021-05-30 06:20:37 +0900

    UI 구현 V2

---
+++ app/src/main/java/com/norispace/noristory/SelectUserActivity.kt
@@ -0,0 +1,11 @@
+package com.norispace.noristory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SelectUserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_user)
+    }
+}

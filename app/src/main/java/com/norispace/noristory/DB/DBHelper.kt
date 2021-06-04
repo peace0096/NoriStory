e271eb5caec6b2f1576a73cd0db1a65392d77f18
peace0096 <peace0096@naver.com>
2021-06-04 17:18:42 +0900

    s3helper upload and sqlite db

--- app/src/main/java/com/norispace/noristory/DB/DBHelper.kt
+++ app/src/main/java/com/norispace/noristory/DB/DBHelper.kt
@@ -1,6 +1,7 @@
 package com.norispace.noristory.DB
 
 import android.app.ProgressDialog
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
@@ -10,6 +11,7 @@ import android.util.Log
 import com.norispace.noristory.Books.BookData
 import com.norispace.noristory.Model.OptionalStory_Model
 import com.norispace.noristory.R
+import com.norispace.noristory.SubjectStoryData
 import com.norispace.service.S3Helper
 import java.io.File
 import java.io.FileOutputStream
@@ -114,6 +116,119 @@ class DBHelper(val context: Context) : SQLiteOpenHelper(context, DB_NAME, null, 
         return data
 
     }
+
+    fun insertCard(card:String) {
+        val values = ContentValues()
+        values.put("name", card)
+        val db = writableDatabase
+        db.insert("Card", null, values)
+        db.close()
+
+    }
+
+    fun deleteCard(card: String) : Boolean {
+        val strsql = "select * from Card where name = '$card';"
+        val db = writableDatabase
+        val cursor = db.rawQuery(strsql, null)
+        val flag = cursor.count != 0
+        if(flag) {
+            cursor.moveToFirst()
+            db.delete("Card", "name=?", arrayOf(card))
+            val cardFile = File(context.cacheDir.toString() + "/" + card)
+            cardFile.delete()
+        }
+        cursor.close()
+        db.close()
+        return flag
+    }
+
+    fun getCards() : ArrayList<String> {
+        val list = ArrayList<String>()
+        val sqlstr = "select * from Card;"
+        val db = readableDatabase
+        val cursor = db.rawQuery(sqlstr, null)
+        val flag = cursor.count != 0
+        if(flag) {
+            cursor.moveToFirst()
+            val name = cursor.getString(cursor.getColumnIndex("name"))
+            list.add(name)
+        }
+        cursor.close()
+        db.close()
+        return list
+    }
+
+    //필요없을 것 같음 배경은 drawable에 넣을거니까 아마?
+//    fun insertBackground(background:String) {
+//        val values = ContentValues()
+//        values.put("name", background)
+//        val db = writableDatabase
+//        db.insert("Background", null, values)
+//        db.close()
+//
+//    }
+
+
+
+    fun insertContent(title:String, data:SubjectStoryData) {
+        //콘텐츠 넣기. 만약 콘텐츠 내용물이 변했다면, delete를 하고 insert하는 것이 빠를 것임.
+        val values = ContentValues()
+        values.put("title", title)
+
+        //이름 추가해야함
+        values.put("page", data.page)
+        values.put("sizeX", data.sizeX)
+        values.put("sizeY", data.sizeY)
+        values.put("locationX", data.locationX)
+        values.put("locationY", data.locationY)
+        values.put("contentType", data.contentType)
+        values.put("content", data.content)
+
+        val db = writableDatabase
+        db.insert("SubjectStoryContent", null, values)
+        db.close()
+    }
+
+    fun deleteContent(title:String, data:SubjectStoryData) : Boolean {
+        //DB안에서만 삭제되므로, 그림판 내에 있는 내용은 따로 삭제해야함.
+        val strsql = "select * from SubjectStoryContent where title = '$title' and page = ${data.page};"
+        val db = writableDatabase
+        val cursor = db.rawQuery(strsql, null)
+        val flag = cursor.count != 0
+        if(flag) {
+            cursor.moveToFirst()
+            db.delete("SubjectStoryContent", "title='$title' and page = ${data.page}", null)
+
+        }
+        cursor.close()
+        db.close()
+        return flag
+
+    }
+
+    fun getContent(title: String) : ArrayList<SubjectStoryData> {
+        val list = ArrayList<SubjectStoryData>()
+        val sqlstr = "select * from SubjectStoryContent;"
+        val db = readableDatabase
+        val cursor = db.rawQuery(sqlstr, null)
+        val flag = cursor.count != 0
+        if(flag) {
+            cursor.moveToFirst()
+            val page = cursor.getInt(cursor.getColumnIndex("page"))
+            val sizeX = cursor.getInt(cursor.getColumnIndex("sizeX"))
+            val sizeY = cursor.getInt(cursor.getColumnIndex("sizeY"))
+            val locationX = cursor.getInt(cursor.getColumnIndex("locationX"))
+            val locationY = cursor.getInt(cursor.getColumnIndex("locationY"))
+            val contentType = cursor.getInt(cursor.getColumnIndex("contentType"))
+            val content = cursor.getString(cursor.getColumnIndex("content"))
+            list.add(SubjectStoryData(page, sizeX, sizeY, locationX, locationY, contentType, content))
+
+        }
+        cursor.close()
+        db.close()
+        return list
+    }
+
 
     fun initDB() {
 
--- app/src/main/java/com/norispace/service/S3Helper.kt
+++ app/src/main/java/com/norispace/service/S3Helper.kt
@@ -51,6 +51,58 @@ class S3Helper(val context:Context) {
         return url
     }
 
+    fun uploadImage(data: ArrayList<String>) {
+        val credentialsProvider = CognitoCachingCredentialsProvider(
+            context,
+            POOLID, // 자격 증명 풀 ID
+            REGION // 리전
+        );
+        TransferNetworkLossHandler.getInstance(context)
+
+        // TransferUtility 객체 생성
+        val transferUtility = TransferUtility.builder()
+            .context(context)
+            .defaultBucket(BUCKET_NAME) // 디폴트 버킷 이름.
+            .s3Client(AmazonS3Client(credentialsProvider, Region.getRegion(REGION)))
+            .build()
+
+        for(i in 0 until data.size) {
+            val uploadObserver = transferUtility.upload(data[i], File(context.cacheDir.toString() + "/" + data[i]))
+            Log.i("upload data", data[i].toString())
+            val progressDialog = ProgressDialog(context)
+            progressDialog.setMessage("다운로드 중...")
+            progressDialog.setCancelable(true)
+            progressDialog.setProgressStyle(android.R.style.Widget_ProgressBar_Horizontal)
+            progressDialog.show()
+
+            uploadObserver.setTransferListener(object : TransferListener {
+                override fun onProgressChanged(id: Int, current: Long, total: Long) {
+                    try {
+                        val done = (((current.toDouble() / total) * 100.0).toInt()) //as Int
+                        Log.d("AWS", "UPLOAD - - ID: $id, percent done = $done")
+                    }
+                    catch (e: Exception) {
+                        Log.d("AWS", "Trouble calculating progress percent", e)
+                    }
+                }
+
+                override fun onStateChanged(id: Int, state: TransferState?) {
+                    if (state == TransferState.COMPLETED) {
+                        progressDialog.dismiss()
+                        Log.d("AWS", "UPLOAD Completed!")
+                    }
+                }
+                override fun onError(id: Int, ex: java.lang.Exception) {
+                    Log.d("AWS", "UPLOAD ERROR - - ID: $id - - EX: ${ex.message.toString()}")
+                }
+
+
+            })
+
+        }
+    }
+
+
 
     fun downloadImage(data: ArrayList<String>) {
 
